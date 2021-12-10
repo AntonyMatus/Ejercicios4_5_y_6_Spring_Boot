9ffdadeebd44b63ec4d6db9c250c75d9a25abec6
tony_ <antonyrebolledo@gmail.com>
2021-12-10 12:57:24 -0600

    Ejercicio 3 resuelto

--- src/main/java/com/example/Ejercicios456/controllers/LaptopController.java
+++ src/main/java/com/example/Ejercicios456/controllers/LaptopController.java
@@ -3,6 +3,8 @@ package com.example.Ejercicios456.controllers;
 import com.example.Ejercicios456.entity.Laptop;
 import com.example.Ejercicios456.repository.LaptopRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,5 +26,16 @@ public class LaptopController {
     @GetMapping("/api/list-laptop")
     public List<Laptop> findAll(){
         return laptopRepository.findAll();
+    }
+
+    //Ejercicio 3
+    // Crear un método en LaptopController que reciba un objeto Laptop enviado en formato JSON desde Postman y persistirlo en la base de datos.
+
+    @PostMapping("/api/list-laptop")
+    public Laptop create(@RequestBody Laptop laptop){
+
+        //Guardar la Laptop recibido por parametro en la base de datos
+
+        return laptopRepository.save(laptop);
     }
 }
