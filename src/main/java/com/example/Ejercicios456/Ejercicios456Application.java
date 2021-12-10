031c81d979008a55a3eeb82dd1a1ce963a203f4e
tony_ <antonyrebolledo@gmail.com>
2021-12-10 11:25:47 -0600

    Ejercicio 1 resuelto

--- src/main/java/com/example/Ejercicios456/Ejercicios456Application.java
+++ src/main/java/com/example/Ejercicios456/Ejercicios456Application.java
@@ -1,14 +1,31 @@
 package com.example.Ejercicios456;
 
+import com.example.Ejercicios456.entity.Laptop;
+import com.example.Ejercicios456.repository.LaptopRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Ejercicios456Application {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(Ejercicios456Application.class, args);
+		ApplicationContext context = SpringApplication.run(Ejercicios456Application.class, args);
+		LaptopRepository repository = context.getBean(LaptopRepository.class);
+
+		//crear Laptop
+
+		Laptop laptop1 = new Laptop(null,"Lenovo","IdeaPad 3 15",15399.00,true);
+		Laptop laptop2 = new Laptop(null,"Lenovo","IdeaPad 3 14",16368.00,true);
+		Laptop laptop3 = new Laptop(null,"Lenovo","IdeaPad Gaming 3i 15",18499.00,true);
+
+		// almacenar las Laptops
+
+		repository.save(laptop1);
+		repository.save(laptop2);
+		repository.save(laptop3);
+
 	}
 
 }
---
+++ src/main/java/com/example/Ejercicios456/repository/LaptopRepository.java
@@ -0,0 +1,9 @@
+package com.example.Ejercicios456.repository;
+
+import com.example.Ejercicios456.entity.Laptop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LaptopRepository extends JpaRepository<Laptop, Long> {
+}
