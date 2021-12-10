245878392554394160574adfdd9d469fe8b6e750
tony_ <antonyrebolledo@gmail.com>
2021-12-10 11:29:43 -0600

    Ejercicio 2 resuelto

--- src/main/java/com/example/Ejercicios456/repository/LaptopRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Ejercicios456.repository;
-
-import com.example.Ejercicios456.entity.Laptop;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LaptopRepository extends JpaRepository<Laptop, Long> {
-}
